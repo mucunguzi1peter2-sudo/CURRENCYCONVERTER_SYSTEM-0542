2482cac42bcdba14b31e38fe009cc07b88ea26b4
Mucunguzi Peter <242353395+mucunguzi1peter2-sudo@users.noreply.github.com>
2026-09-21 13:08:25 +0300

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,363 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        lblKabbyaForexBureauConverterSystem = New Label()
+        lblUGX1 = New Label()
+        lblYuan1 = New Label()
+        lblYuan2 = New Label()
+        lblUGX2 = New Label()
+        lblUGX3 = New Label()
+        lblDirham1 = New Label()
+        lblDirham2 = New Label()
+        lblUGX4 = New Label()
+        txtUGXToYuan = New TextBox()
+        txtYuanToUGX = New TextBox()
+        txtYuan = New TextBox()
+        txtUGXFromYuan = New TextBox()
+        txtDirham = New TextBox()
+        txtUGXToDirham = New TextBox()
+        txtDirhamToUGX = New TextBox()
+        txtUGXFromDirham = New TextBox()
+        btnConvertToYuan = New Button()
+        btnConvertToUGXYuan = New Button()
+        btnConvertToDirham = New Button()
+        btnConvertToUGXDirham = New Button()
+        btnClear = New Button()
+        Panel1 = New Panel()
+        Panel2 = New Panel()
+        btnExit = New Button()
+        Panel1.SuspendLayout()
+        Panel2.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' lblKabbyaForexBureauConverterSystem
+        ' 
+        lblKabbyaForexBureauConverterSystem.AutoSize = True
+        lblKabbyaForexBureauConverterSystem.Font = New Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        lblKabbyaForexBureauConverterSystem.Location = New Point(132, 12)
+        lblKabbyaForexBureauConverterSystem.Name = "lblKabbyaForexBureauConverterSystem"
+        lblKabbyaForexBureauConverterSystem.Size = New Size(535, 32)
+        lblKabbyaForexBureauConverterSystem.TabIndex = 0
+        lblKabbyaForexBureauConverterSystem.Text = "KABBYA FOREX BUREAU CONVERTER SYSTEM"
+        ' 
+        ' lblUGX1
+        ' 
+        lblUGX1.AutoSize = True
+        lblUGX1.Font = New Font("Arial Rounded MT Bold", 15.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        lblUGX1.Location = New Point(80, 11)
+        lblUGX1.Name = "lblUGX1"
+        lblUGX1.Size = New Size(56, 24)
+        lblUGX1.TabIndex = 1
+        lblUGX1.Text = "UGX"
+        ' 
+        ' lblYuan1
+        ' 
+        lblYuan1.AutoSize = True
+        lblYuan1.Font = New Font("Arial Rounded MT Bold", 15.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        lblYuan1.Location = New Point(467, 11)
+        lblYuan1.Name = "lblYuan1"
+        lblYuan1.Size = New Size(172, 24)
+        lblYuan1.TabIndex = 2
+        lblYuan1.Text = "CHINESE YUAN"
+        ' 
+        ' lblYuan2
+        ' 
+        lblYuan2.AutoSize = True
+        lblYuan2.Font = New Font("Arial Rounded MT Bold", 15.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        lblYuan2.Location = New Point(32, 119)
+        lblYuan2.Name = "lblYuan2"
+        lblYuan2.Size = New Size(172, 24)
+        lblYuan2.TabIndex = 3
+        lblYuan2.Text = "CHINESE YUAN"
+        ' 
+        ' lblUGX2
+        ' 
+        lblUGX2.AutoSize = True
+        lblUGX2.Font = New Font("Arial Rounded MT Bold", 15.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        lblUGX2.Location = New Point(531, 115)
+        lblUGX2.Name = "lblUGX2"
+        lblUGX2.Size = New Size(56, 24)
+        lblUGX2.TabIndex = 4
+        lblUGX2.Text = "UGX"
+        ' 
+        ' lblUGX3
+        ' 
+        lblUGX3.AutoSize = True
+        lblUGX3.Font = New Font("Arial Rounded MT Bold", 15.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        lblUGX3.Location = New Point(82, 12)
+        lblUGX3.Name = "lblUGX3"
+        lblUGX3.Size = New Size(56, 24)
+        lblUGX3.TabIndex = 5
+        lblUGX3.Text = "UGX"
+        ' 
+        ' lblDirham1
+        ' 
+        lblDirham1.AutoSize = True
+        lblDirham1.Font = New Font("Arial Rounded MT Bold", 15.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        lblDirham1.Location = New Point(490, 11)
+        lblDirham1.Name = "lblDirham1"
+        lblDirham1.Size = New Size(146, 24)
+        lblDirham1.TabIndex = 6
+        lblDirham1.Text = "UAE DIRHAM"
+        ' 
+        ' lblDirham2
+        ' 
+        lblDirham2.AutoSize = True
+        lblDirham2.Font = New Font("Arial Rounded MT Bold", 15.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        lblDirham2.Location = New Point(47, 112)
+        lblDirham2.Name = "lblDirham2"
+        lblDirham2.Size = New Size(146, 24)
+        lblDirham2.TabIndex = 7
+        lblDirham2.Text = "UAE DIRHAM"
+        ' 
+        ' lblUGX4
+        ' 
+        lblUGX4.AutoSize = True
+        lblUGX4.Font = New Font("Arial Rounded MT Bold", 15.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        lblUGX4.Location = New Point(531, 117)
+        lblUGX4.Name = "lblUGX4"
+        lblUGX4.Size = New Size(56, 24)
+        lblUGX4.TabIndex = 8
+        lblUGX4.Text = "UGX"
+        ' 
+        ' txtUGXToYuan
+        ' 
+        txtUGXToYuan.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        txtUGXToYuan.Location = New Point(23, 45)
+        txtUGXToYuan.Name = "txtUGXToYuan"
+        txtUGXToYuan.Size = New Size(190, 29)
+        txtUGXToYuan.TabIndex = 11
+        ' 
+        ' txtYuanToUGX
+        ' 
+        txtYuanToUGX.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        txtYuanToUGX.Location = New Point(23, 154)
+        txtYuanToUGX.Name = "txtYuanToUGX"
+        txtYuanToUGX.Size = New Size(188, 29)
+        txtYuanToUGX.TabIndex = 12
+        ' 
+        ' txtYuan
+        ' 
+        txtYuan.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        txtYuan.Location = New Point(449, 45)
+        txtYuan.Name = "txtYuan"
+        txtYuan.Size = New Size(207, 29)
+        txtYuan.TabIndex = 13
+        ' 
+        ' txtUGXFromYuan
+        ' 
+        txtUGXFromYuan.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        txtUGXFromYuan.Location = New Point(449, 154)
+        txtUGXFromYuan.Name = "txtUGXFromYuan"
+        txtUGXFromYuan.Size = New Size(207, 29)
+        txtUGXFromYuan.TabIndex = 14
+        ' 
+        ' txtDirham
+        ' 
+        txtDirham.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        txtDirham.Location = New Point(463, 46)
+        txtDirham.Name = "txtDirham"
+        txtDirham.Size = New Size(193, 29)
+        txtDirham.TabIndex = 15
+        ' 
+        ' txtUGXToDirham
+        ' 
+        txtUGXToDirham.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        txtUGXToDirham.Location = New Point(23, 46)
+        txtUGXToDirham.Name = "txtUGXToDirham"
+        txtUGXToDirham.Size = New Size(204, 29)
+        txtUGXToDirham.TabIndex = 16
+        ' 
+        ' txtDirhamToUGX
+        ' 
+        txtDirhamToUGX.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        txtDirhamToUGX.Location = New Point(23, 147)
+        txtDirhamToUGX.Name = "txtDirhamToUGX"
+        txtDirhamToUGX.Size = New Size(204, 29)
+        txtDirhamToUGX.TabIndex = 17
+        ' 
+        ' txtUGXFromDirham
+        ' 
+        txtUGXFromDirham.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        txtUGXFromDirham.Location = New Point(461, 152)
+        txtUGXFromDirham.Name = "txtUGXFromDirham"
+        txtUGXFromDirham.Size = New Size(195, 29)
+        txtUGXFromDirham.TabIndex = 18
+        ' 
+        ' btnConvertToYuan
+        ' 
+        btnConvertToYuan.BackColor = SystemColors.ControlText
+        btnConvertToYuan.Font = New Font("Times New Roman", 15.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        btnConvertToYuan.ForeColor = Color.White
+        btnConvertToYuan.Location = New Point(247, 16)
+        btnConvertToYuan.Name = "btnConvertToYuan"
+        btnConvertToYuan.Size = New Size(179, 72)
+        btnConvertToYuan.TabIndex = 19
+        btnConvertToYuan.Text = "CONVERT TO YUAN"
+        btnConvertToYuan.UseVisualStyleBackColor = False
+        ' 
+        ' btnConvertToUGXYuan
+        ' 
+        btnConvertToUGXYuan.BackColor = Color.Black
+        btnConvertToUGXYuan.Font = New Font("Times New Roman", 15.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        btnConvertToUGXYuan.ForeColor = Color.White
+        btnConvertToUGXYuan.Location = New Point(247, 124)
+        btnConvertToUGXYuan.Name = "btnConvertToUGXYuan"
+        btnConvertToUGXYuan.Size = New Size(179, 76)
+        btnConvertToUGXYuan.TabIndex = 20
+        btnConvertToUGXYuan.Text = "CONVERT TO UGX"
+        btnConvertToUGXYuan.UseVisualStyleBackColor = False
+        ' 
+        ' btnConvertToDirham
+        ' 
+        btnConvertToDirham.BackColor = Color.Black
+        btnConvertToDirham.Font = New Font("Times New Roman", 15.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        btnConvertToDirham.ForeColor = Color.White
+        btnConvertToDirham.Location = New Point(247, 11)
+        btnConvertToDirham.Name = "btnConvertToDirham"
+        btnConvertToDirham.Size = New Size(179, 72)
+        btnConvertToDirham.TabIndex = 21
+        btnConvertToDirham.Text = "CONVERT TO DIRHAM"
+        btnConvertToDirham.UseVisualStyleBackColor = False
+        ' 
+        ' btnConvertToUGXDirham
+        ' 
+        btnConvertToUGXDirham.BackColor = Color.Black
+        btnConvertToUGXDirham.Font = New Font("Times New Roman", 15.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        btnConvertToUGXDirham.ForeColor = Color.White
+        btnConvertToUGXDirham.Location = New Point(247, 110)
+        btnConvertToUGXDirham.Name = "btnConvertToUGXDirham"
+        btnConvertToUGXDirham.Size = New Size(179, 74)
+        btnConvertToUGXDirham.TabIndex = 22
+        btnConvertToUGXDirham.Text = "CONVERT TO UGX"
+        btnConvertToUGXDirham.UseVisualStyleBackColor = False
+        ' 
+        ' btnClear
+        ' 
+        btnClear.BackColor = Color.Black
+        btnClear.Font = New Font("Times New Roman", 18F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btnClear.ForeColor = Color.White
+        btnClear.Location = New Point(526, 509)
+        btnClear.Name = "btnClear"
+        btnClear.Size = New Size(166, 46)
+        btnClear.TabIndex = 23
+        btnClear.Text = "CLEAR"
+        btnClear.UseVisualStyleBackColor = False
+        ' 
+        ' Panel1
+        ' 
+        Panel1.BackColor = Color.Silver
+        Panel1.Controls.Add(btnConvertToUGXYuan)
+        Panel1.Controls.Add(btnConvertToYuan)
+        Panel1.Controls.Add(txtUGXFromYuan)
+        Panel1.Controls.Add(txtYuan)
+        Panel1.Controls.Add(txtYuanToUGX)
+        Panel1.Controls.Add(txtUGXToYuan)
+        Panel1.Controls.Add(lblUGX2)
+        Panel1.Controls.Add(lblYuan2)
+        Panel1.Controls.Add(lblYuan1)
+        Panel1.Controls.Add(lblUGX1)
+        Panel1.Location = New Point(61, 61)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(697, 216)
+        Panel1.TabIndex = 24
+        ' 
+        ' Panel2
+        ' 
+        Panel2.BackColor = Color.Silver
+        Panel2.Controls.Add(btnConvertToUGXDirham)
+        Panel2.Controls.Add(btnConvertToDirham)
+        Panel2.Controls.Add(txtUGXFromDirham)
+        Panel2.Controls.Add(txtDirhamToUGX)
+        Panel2.Controls.Add(txtUGXToDirham)
+        Panel2.Controls.Add(txtDirham)
+        Panel2.Controls.Add(lblUGX4)
+        Panel2.Controls.Add(lblDirham2)
+        Panel2.Controls.Add(lblDirham1)
+        Panel2.Controls.Add(lblUGX3)
+        Panel2.Location = New Point(61, 302)
+        Panel2.Name = "Panel2"
+        Panel2.Size = New Size(697, 197)
+        Panel2.TabIndex = 25
+        ' 
+        ' btnExit
+        ' 
+        btnExit.BackColor = Color.Black
+        btnExit.Font = New Font("Times New Roman", 18F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btnExit.ForeColor = Color.White
+        btnExit.Location = New Point(145, 509)
+        btnExit.Name = "btnExit"
+        btnExit.Size = New Size(166, 46)
+        btnExit.TabIndex = 26
+        btnExit.Text = "EXIT"
+        btnExit.UseVisualStyleBackColor = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.BurlyWood
+        ClientSize = New Size(828, 582)
+        Controls.Add(btnExit)
+        Controls.Add(Panel2)
+        Controls.Add(Panel1)
+        Controls.Add(btnClear)
+        Controls.Add(lblKabbyaForexBureauConverterSystem)
+        Name = "Form1"
+        Text = "KABBYA FOREX BUREAU"
+        Panel1.ResumeLayout(False)
+        Panel1.PerformLayout()
+        Panel2.ResumeLayout(False)
+        Panel2.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents lblKabbyaForexBureauConverterSystem As Label
+    Friend WithEvents lblUGX1 As Label
+    Friend WithEvents lblYuan1 As Label
+    Friend WithEvents lblYuan2 As Label
+    Friend WithEvents lblUGX2 As Label
+    Friend WithEvents lblUGX3 As Label
+    Friend WithEvents lblDirham1 As Label
+    Friend WithEvents lblDirham2 As Label
+    Friend WithEvents lblUGX4 As Label
+    Friend WithEvents txtUGXToYuan As TextBox
+    Friend WithEvents txtYuanToUGX As TextBox
+    Friend WithEvents txtYuan As TextBox
+    Friend WithEvents txtUGXFromYuan As TextBox
+    Friend WithEvents txtDirham As TextBox
+    Friend WithEvents txtUGXToDirham As TextBox
+    Friend WithEvents txtDirhamToUGX As TextBox
+    Friend WithEvents txtUGXFromDirham As TextBox
+    Friend WithEvents btnConvertToYuan As Button
+    Friend WithEvents btnConvertToUGXYuan As Button
+    Friend WithEvents btnConvertToDirham As Button
+    Friend WithEvents btnConvertToUGXDirham As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents btnExit As Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,305 @@
+﻿Public Class Form1
+    Const UGX_TO_YUAN_RATE As Decimal = 540D
+    Const UGX_TO_DIRHAM_RATE As Decimal = 110D
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Clear all text boxes when the application starts
+        txtUGXToYuan.Clear()
+        txtYuan.Clear()
+        txtYuanToUGX.Clear()
+        txtUGXFromYuan.Clear()
+
+        txtUGXToDirham.Clear()
+        txtDirham.Clear()
+        txtDirhamToUGX.Clear()
+        txtUGXFromDirham.Clear()
+
+        'Make output fields read-only
+        txtYuan.ReadOnly = True
+        txtUGXFromYuan.ReadOnly = True
+        txtDirham.ReadOnly = True
+        txtUGXFromDirham.ReadOnly = True
+
+        'Set the cursor to the first input field
+        txtUGXToYuan.Focus()
+
+    End Sub
+
+    Private Sub btnConvertToYuan_Click(sender As Object, e As EventArgs) Handles btnConvertToYuan.Click
+        Dim UGXAmount As Decimal
+        Dim YuanAmount As Decimal
+
+        'Check whether the user entered an amount
+        If txtUGXToYuan.Text.Trim() = "" Then
+
+            MessageBox.Show(
+                "Please enter the amount in UGX.",
+                "Input Required",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Warning)
+
+            txtUGXToYuan.Focus()
+            Exit Sub
+
+        End If
+
+
+        'Check whether the entered value is a valid number
+        If Not Decimal.TryParse(txtUGXToYuan.Text, UGXAmount) Then
+
+            MessageBox.Show(
+                "Please enter a valid numeric amount.",
+                "Invalid Input",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Error)
+
+            txtUGXToYuan.SelectAll()
+            txtUGXToYuan.Focus()
+            Exit Sub
+
+        End If
+
+
+        'Check for negative amount
+        If UGXAmount < 0 Then
+
+            MessageBox.Show(
+                "Amount cannot be negative.",
+                "Invalid Amount",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Warning)
+
+            txtUGXToYuan.SelectAll()
+            txtUGXToYuan.Focus()
+            Exit Sub
+
+        End If
+
+
+        'Perform conversion
+        YuanAmount = UGXAmount / UGX_TO_YUAN_RATE
+
+
+        'Display result
+        txtYuan.Text = YuanAmount.ToString("N2")
+
+    End Sub
+
+    Private Sub btnConvertToUGXYuan_Click(sender As Object, e As EventArgs) Handles btnConvertToUGXYuan.Click
+        Dim yuanAmount As Decimal
+        Dim ugxAmount As Decimal
+
+        'Check whether amount has been entered
+        If txtUGXToYuan.Text.Trim() = "" Then
+
+            MessageBox.Show(
+                "Please enter the amount in Chinese Yuan.",
+                "Input Required",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Warning)
+
+            txtUGXToYuan.Focus()
+            Exit Sub
+
+        End If
+
+
+        'Validate input
+        If Not Decimal.TryParse(txtUGXToYuan.Text, yuanAmount) Then
+
+            MessageBox.Show(
+                "Please enter a valid numeric amount.",
+                "Invalid Input",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Error)
+
+            txtUGXToYuan.SelectAll()
+            txtUGXToYuan.Focus()
+            Exit Sub
+
+        End If
+
+
+        'Check for negative value
+        If yuanAmount < 0 Then
+
+            MessageBox.Show(
+                "Amount cannot be negative.",
+                "Invalid Amount",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Warning)
+
+            txtUGXToYuan.SelectAll()
+            txtUGXToYuan.Focus()
+            Exit Sub
+
+        End If
+
+
+        'Perform conversion
+        ugxAmount = yuanAmount * UGX_TO_YUAN_RATE
+
+
+        'Display result
+        txtUGXFromYuan.Text = ugxAmount.ToString("N2")
+
+    End Sub
+
+    Private Sub btnConvertToDirham_Click(sender As Object, e As EventArgs) Handles btnConvertToDirham.Click
+        Dim UGXAmount As Decimal
+        Dim DirhamAmount As Decimal
+
+        'Check whether amount has been entered
+        If txtUGXToDirham.Text.Trim() = "" Then
+
+            MessageBox.Show(
+                "Please enter the amount in UGX.",
+                "Input Required",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Warning)
+
+            txtUGXToDirham.Focus()
+            Exit Sub
+
+        End If
+
+
+        'Validate input
+        If Not Decimal.TryParse(txtUGXToDirham.Text, ugxAmount) Then
+
+            MessageBox.Show(
+                "Please enter a valid numeric amount.",
+                "Invalid Input",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Error)
+
+            txtUGXToDirham.SelectAll()
+            txtUGXToDirham.Focus()
+            Exit Sub
+
+        End If
+
+
+        'Check for negative amount
+        If ugxAmount < 0 Then
+
+            MessageBox.Show(
+                "Amount cannot be negative.",
+                "Invalid Amount",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Warning)
+
+            txtUGXToDirham.SelectAll()
+            txtUGXToDirham.Focus()
+            Exit Sub
+
+        End If
+
+
+        'Perform conversion
+        DirhamAmount = UGXAmount / UGX_TO_DIRHAM_RATE
+
+
+        'Display result
+        txtDirham.Text = DirhamAmount.ToString("N2")
+
+    End Sub
+
+    Private Sub btnConvertToUGXDirham_Click(sender As Object, e As EventArgs) Handles btnConvertToUGXDirham.Click
+        Dim dirhamAmount As Decimal
+        Dim ugxAmount As Decimal
+
+        'Check whether amount has been entered
+        If txtDirhamToUGX.Text.Trim() = "" Then
+
+            MessageBox.Show(
+                "Please enter the amount in UAE Dirham.",
+                "Input Required",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Warning)
+
+            txtDirhamToUGX.Focus()
+            Exit Sub
+
+        End If
+
+
+        'Validate input
+        If Not Decimal.TryParse(txtDirhamToUGX.Text, dirhamAmount) Then
+
+            MessageBox.Show(
+                "Please enter a valid numeric amount.",
+                "Invalid Input",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Error)
+
+            txtDirhamToUGX.SelectAll()
+            txtDirhamToUGX.Focus()
+            Exit Sub
+
+        End If
+
+
+        'Check for negative value
+        If dirhamAmount < 0 Then
+
+            MessageBox.Show(
+                "Amount cannot be negative.",
+                "Invalid Amount",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Warning)
+
+            txtDirhamToUGX.SelectAll()
+            txtDirhamToUGX.Focus()
+            Exit Sub
+
+        End If
+
+
+        'Perform conversion
+        ugxAmount = dirhamAmount * UGX_TO_DIRHAM_RATE
+
+
+        'Display result
+        txtUGXFromDirham.Text = ugxAmount.ToString("N2")
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        'Clear UGX/Yuan conversion
+        txtUGXToYuan.Clear()
+        txtYuan.Clear()
+
+        'Clear Yuan/UGX conversion
+        txtYuanToUGX.Clear()
+        txtUGXFromYuan.Clear()
+
+        'Clear UGX/Dirham conversion
+        txtUGXToDirham.Clear()
+        txtDirham.Clear()
+
+        'Clear Dirham/UGX conversion
+        txtDirhamToUGX.Clear()
+        txtUGXFromDirham.Clear()
+
+        'Return cursor to first field
+        txtUGXToYuan.Focus()
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Dim answer As DialogResult
+
+        answer = MessageBox.Show(
+            "Are you sure you want to exit Kabbya Forex Bureau?",
+            "Exit Application",
+            MessageBoxButtons.YesNo,
+            MessageBoxIcon.Question)
+
+        If answer = DialogResult.Yes Then
+
+            Application.Exit()
+
+        End If
+
+    End Sub
+End Class
